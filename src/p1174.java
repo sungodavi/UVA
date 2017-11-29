16b68a981aeaf9d6c971be7f2b7fb46ad081dc9c
sungodavi <sungodavi@gmail.com>
2017-11-28 23:20:35 -0600

    Commit

--- src/p1174.java
+++ src/p1174.java
@@ -4,10 +4,54 @@ import java.io.*;
 public class p1174 
 {
 	static HashMap<String, Integer> map;
+	static ArrayList<Edge> list;
+	static int count;
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		int times = Integer.parseInt(f.readLine());
+		while(times-- > 0)
+		{
+			f.readLine();
+			map = new HashMap<String, Integer>();
+			int size = Integer.parseInt(f.readLine());
+			int edges = Integer.parseInt(f.readLine());
+			count = 0;
+			list = new ArrayList<Edge>(size);
+			for(int i = 1; i <= edges; i++)
+			{
+				st = new StringTokenizer(f.readLine());
+				String u = st.nextToken();
+				String v = st.nextToken();
+				add(u);
+				add(v);
+				int weight = Integer.parseInt(st.nextToken());
+				list.add(new Edge(map.get(u), map.get(v), weight));
+			}
+			System.out.println(krusts(size));
+			if(times > 0)
+				System.out.println();
+		}
+	}
+	
+	static int krusts(int size)
+	{
+		UnionFind uf = new UnionFind(size);
+		Collections.sort(list);
+		int result = 0;
+		for(int i = 0; i < list.size() && uf.sets > 1; i++)
+		{
+			Edge e = list.get(i);
+			if(uf.union(e.u, e.v))
+				result += e.weight;
+		}
+		return result;
+	}
+	static void add(String s)
+	{
+		if(!map.containsKey(s))
+			map.put(s, count++);
 	}
 	static class UnionFind
 	{
@@ -51,6 +95,23 @@ public class p1174
 					rank[p]++;
 			}
 			return true;
+		}
+	}
+	
+	static class Edge implements Comparable<Edge>
+	{
+		int u, v, weight;
+		
+		public Edge(int u, int v, int weight)
+		{
+			this.u = u;
+			this.v = v;
+			this.weight = weight;
+		}
+		
+		public int compareTo(Edge e)
+		{
+			return weight - e.weight;
 		}
 	}
 }
---
+++ src/p1208.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+public class p1208 
+{
+	static int[][] a;
+	static ArrayList<Edge> list;
+	public static void main(String[] args) throws IOException 
+	{
+		System.setOut(new PrintStream("output.txt"));
+		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+		int times = Integer.parseInt(f.readLine());
+		int count = 1;
+		while(times-- > 0)
+		{
+			 int size = Integer.parseInt(f.readLine());
+			 a = new int[size][size];
+			 for(int r = 0; r < a.length; r++)
+			 {
+				 StringTokenizer st = new StringTokenizer(f.readLine(), ", ");
+				 for(int c = 0; c < a.length; c++)
+					 a[r][c] = Integer.parseInt(st.nextToken());
+			 }
+			 list = new ArrayList<Edge>();
+			 prims();
+			 Collections.sort(list);
+			 System.out.printf("Case %d:\n", count++);
+			 for(Edge e: list)
+				 System.out.println(e);
+		}
+	}
+	
+	static void prims()
+	{
+		Queue<Edge> q = new PriorityQueue<Edge>();
+		for(int c = 0; c < a.length; c++)
+			if(a[0][c] > 0)
+				q.add(new Edge(0, c, a[0][c]));
+		boolean[] visited = new boolean[a.length];
+		visited[0] = true;
+		while(!q.isEmpty())
+		{
+			Edge e = q.poll();
+			list.add(e);
+			visited[e.v] = true;
+			for(int c = 0; c < a.length; c++)
+			{
+				if(a[e.v][c] > 0 && !visited[c])
+					q.add(new Edge(e.v, c, a[e.v][c]));
+			}
+		}
+	}
+	
+	
+	
+	static class Edge implements Comparable<Edge>
+	{
+		int u, v, weight;
+		
+		public Edge(int u, int v, int weight)
+		{
+			this.u = u;
+			this.v = v;
+			this.weight = weight;
+		}
+		
+		public int compareTo(Edge e)
+		{
+			if(weight == e.weight)
+			{
+				if(Math.min(u, v) == Math.min(e.u, e.v))
+					return Math.max(u, v) - Math.max(e.u, e.v);
+				return Math.min(u, v) - Math.min(e.u, e.v);
+					
+			}
+			return weight - e.weight;
+		}
+		
+		public String toString()
+		{
+			return String.format("%c-%c %d", (char)(Math.min(u, v) + 'A'), (char)(Math.max(u, v) + 'A'), weight);
+		}
+	}
+}
